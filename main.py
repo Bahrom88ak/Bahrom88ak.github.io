43dfaf1f640dea91dcc12c2cfe7f609f11b06b65
Bahrom <111111686+Bahrom88ak@users.noreply.github.com>
2024-04-11 01:52:34 +0500

    main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def web_app(callback_query):
     await bot.send_message(callback_query.from_user.id, f""")
 {message}
 """)
-    await bot.send_message('6005830556', f"""
+    await bot.send_message('6690320111', f"""
 Новый Заказ
 {message}
 """)
