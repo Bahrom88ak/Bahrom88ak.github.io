eb9a3231feb0c18ef1fc2e8ff9919e63e5956bc1
Bahrom <111111686+Bahrom88ak@users.noreply.github.com>
2024-04-11 17:48:19 +0500

    app.js

--- app.js
+++ app.js
@@ -90,7 +90,6 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.first_name}
-${tg.initDataUnsafe.user_id} ${tg.initDataUnsafe.last_name}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
